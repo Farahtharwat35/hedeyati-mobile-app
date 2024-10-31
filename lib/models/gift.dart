12483134526e709b818f84aa22dc247431176809
Farah Tharwat <65964974+Farahtharwat35@users.noreply.github.com>
2024-10-31 10:35:23 +0300

    modified gift model

--- lib/models/gift.dart
+++ lib/models/gift.dart
@@ -1,13 +1,20 @@
+import 'package:sqflite/sqflite.dart';
+import 'dart:async';
+import 'package:flutter/widgets.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
 class Gift {
-  final String description;
-  final String imageUrl;
-  final String price;
+  final int id;
+  String description ;
+  String imageUrl;
+  String price;
   bool isPledged;
 
   Gift({
+    required this.id,
     required this.description,
     required this.imageUrl,
     required this.price,
     this.isPledged = false,
   });
-}
