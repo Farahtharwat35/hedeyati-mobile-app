901335e5605e19d1c6553a5a4f8181723b1e05bb
Farah <farahtharwat35@gmail.com>
2024-12-16 19:07:17 +0200

    fixed uniqueness on phone validation while registration

--- lib/helpers/user_data_uniqueness_validator.dart
+++ lib/helpers/user_data_uniqueness_validator.dart
@@ -7,7 +7,7 @@ Future<bool> userDataUniquenessValidator(
     required String phone} ) async {
   log('***********Checking user data uniqueness***********');
   final result1  = await userCRUD.getWhere([{'username': QueryArg(isEqualTo: username)}]);
-  final result2  = await userCRUD.getWhere([{'phone': QueryArg(isEqualTo: phone)}]);
+  final result2  = await userCRUD.getWhere([{'phoneNumber': QueryArg(isEqualTo: phone)}]);
   bool areValid = result1.isEmpty && result2.isEmpty;
   log('***********User data uniqueness: $areValid***********');
   return areValid;
